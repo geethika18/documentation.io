d86ecfde01fbd40e24c67ea6aa9fbfa8a88e21f2
geethika18 <geethika18kotipalli@gmail.com>
2018-11-14 11:47:01 +0530

    Delete documentation.bat

--- documentation.bat
+++
@@ -1,7 +0,0 @@
-CALL npm install -g typedoc
-if exist customized RD /S /Q customized
-if exist default RD /S /Q default
-if exist minimal RD /S /Q minimal
-CALL typedoc --out customized ../Core/ts --options customTypedoc.js
-CALL typedoc --out default ../Core/ts
-CALL typedoc --out minimal ../Core/ts --theme minimal
